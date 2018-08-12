b2766d87b4ffd159f08540999a11f68111310fe2
jamespine <32987599+jamespine@users.noreply.github.com>
2018-08-12 10:01:38 +0100

    like some stuff

---
+++ client/src/screens/landing.vue
@@ -0,0 +1,179 @@
+<template>
+    <div class="container">
+
+                <transition name="fade">
+                    <div v-if="usernameModal" @click="close" class="backdrop"></div>
+                </transition>
+
+                <transition name="slideup">
+                  <div v-if="usernameModal" class="modal"> 
+                    <div class="modal_content">
+                    <h2>Oh hi mark!</h2>
+                      <div class="dropshadow biginput">
+                        <input v-model="username" type="text" placeholder="Type username here"/>
+                      </div>
+                      <div @click="createUsername" class="button dropshadow skew blue"><span class="noselect"> Go </span></div>
+                  </div>
+                  </div>
+                </transition>
+
+
+      <div class="logo">ChangeMyMind<span>.io</span></div>
+    <div class="dropshadow biginput">
+      <input v-model="question" type="text" placeholder="Type something controversial"/>
+    </div>
+
+    <div class="landing_actions">
+      <div @click="createRoomButton" class="button dropshadow skew red"><span class="noselect"> Create Room </span></div>
+      <h3 class="or">OR</h3>
+      <div @click="joinRoomButton" class="button dropshadow skew blue"><span class="noselect"> Join Random Room </span></div>
+   </div>
+
+   <div class="landing_info">
+     <p>Enter a controversial statment and get matched up with random people for a debate, as the conversation progresses, we’ll throw more randoms into your room to make things more intresting.</p>
+   </div>
+
+
+
+
+
+    </div>
+
+</template>
+
+<script>
+export default {
+  mounted() {
+
+     // in case we were already in a channel
+        console.log('Leaving channel...')
+        if (this.socket) this.socket.send(JSON.stringify({
+          type: "LEAVE_CHANNEL"
+        }))
+
+    // esablish connection with socket
+    this.socket = new WebSocket('wss://56ce6fe7.ngrok.io/ws');
+    this.socket.addEventListener('open', event => {
+      console.log(`Connection established via socket`);
+      this.connectionAlive = true
+    })
+    
+    // Anything coming back from server is handled here
+    this.socket.addEventListener('message', event => {
+          var data = JSON.parse(event.data)
+          console.log(data)
+
+          if (data.type === 'SET_USERNAME') {
+            this.joinOrCreateRoom()
+          }
+
+          if (data.type === 'JOIN_CHANNEL') {
+            this.$router.push('/room')
+          }
+
+          if (data.type === 'RESPONSE') {
+            alert('no free channels :(')
+            this.usernameModal = false
+          }
+
+          this.$root.$emit('response', data)
+    });
+       
+
+    window.socket = this.socket
+
+
+  },
+  data() {
+    return {
+      socket: null,
+      connectionAlive: false,
+      username: '',
+      question: '',
+      usernameModal: false,
+      whatAreWeDoing: false, // JOIN, CREATE
+
+
+    }
+  },
+
+    methods: {
+      createRoomButton() {
+        if (this.question.length < 3) return false
+        this.whatAreWeDoing = 'CREATE'
+        this.usernameModal = true
+      },
+      joinRoomButton() {
+         this.whatAreWeDoing = 'JOIN'
+          this.usernameModal = true
+      },
+      createUsernameModal(type) {
+        this.usernameModalCreateRoom = true;
+      },
+
+      createUsername() {
+          this.socket.send(JSON.stringify({
+          type: "SET_USERNAME",
+          data: {
+            username: this.username
+          }
+        }))
+      },
+
+      // called after response from server on SET_USERNAME
+      joinOrCreateRoom() {
+        if (!this.connectionAlive) return false
+        // CREATE
+        if (this.whatAreWeDoing === 'CREATE') {
+            if (this.username.length < 1) return false 
+
+            this.socket.send(JSON.stringify({
+              type: "CREATE_CHANNEL",
+              data: {
+                topic: this.question
+              }
+            }))
+            this.$router.push('/room')
+        }
+        // JOIN
+        if (this.whatAreWeDoing === 'JOIN') {
+
+          this.socket.send(JSON.stringify({
+            type: "JOIN_CHANNEL"
+          }))
+
+        }
+      },
+
+      close() {
+        this.usernameModal = false
+      }
+
+    }
+}
+</script>
+
+
+
+<style lang="scss">
+.container {
+    margin-top: 60px;
+}
+
+.landing_actions {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  .or {
+    margin: 0 40px;
+    margin-top: 34px;
+    text-align: center;
+    font-size: 1.5rem;
+  }
+}
+.landing_info {
+  width: 600px;
+  margin: auto;
+  margin-top: 70px;
+}
+</style>
---
+++ client/src/screens/room.vue
@@ -0,0 +1,208 @@
+<template>
+    <div class="container_wide room">
+
+
+    <router-link to="/"><div class="logo small">ChangeMyMind<span>.io</span></div></router-link>
+
+    <div class="biginput dropshadow">
+        <input type="text" disabled :value="topic"/>
+        <div class="vote">
+            <div :class="{'notselected': myVote === true }" @click="vote(false)" class="button dropshadow skew red"><span class="noselect"> <div class="fa fa-angle-down"/> </span></div>
+            <div :class="{'notselected': myVote === false }" @click="vote(true)" class="button dropshadow skew blue "><span class="noselect"> <div class="fa fa-angle-up"/> </span></div>
+        <!-- add notselected class when user has made selection -->
+        </div>
+    </div>
+
+    <div class="message_container dropshadow">
+         <div class="message_body  alert_message">
+            <div class="timestamp">{{niceTime(new Date())}}</div><div style="margin-left: 24px" class=" message"> Welcome to the chat!</div>
+         </div>
+        <div v-for="(i, index) in messages" :key="index">
+            <div v-if="i.type === 'NEW_MESSAGE'" class="message_body">
+                <div class="timestamp">{{niceTime(i.timestamp)}}</div>
+
+                <div :class="{'redText': i.member.vote === -1, 'blueText': i.member.vote === 1 }" class="username">{{i.member.username}}</div>
+
+                <div class="message">{{i.message}}</div>
+            </div>
+            <div v-if="i.type === 'MEMBER_JOIN'" class="message_body member_join alert_message">
+                <div class="timestamp">{{niceTime(i.timestamp)}}</div>
+                <div class="username">{{i.username}}</div>
+                <div class="message">has joined the debate</div>
+            </div>
+        </div>
+        <div class="bottom_padding"></div>
+    </div>
+    <div class="dropshadow biginput messageBox">
+      <input v-shortkey="['enter']" @shortkey="sendMessage()" v-model="message" type="text" placeholder="Be rational... or not."/>
+    </div>
+    
+
+    </div>
+</template>
+
+<script>
+import moment from 'moment'
+import momentDurationFormatSetup from 'moment-duration-format'
+
+export default {
+    mounted() {
+
+        this.$root.$on('response', (response) => {
+          if (response.type === 'JOIN_CHANNEL') {
+            this.topic = response.data.topic
+          }
+          if (response.type === 'NEW_MESSAGE') {
+            response.data.type = "NEW_MESSAGE"
+             this.messages.push(response.data)
+          }
+          if (response.type === 'MEMBER_JOIN') {
+            response.data.type = "MEMBER_JOIN"
+            this.messages.push(response.data)
+          }
+        })
+
+    },
+    data() {
+        return {
+            socket: null,
+            topic: '',
+            message: '',
+            messages: [],
+            myVote: null
+        }
+    },
+    methods: {
+        sendMessage() {
+            if (this.message.length < 1) return false
+          window.socket.send(JSON.stringify({
+            type: "SEND_MESSAGE",
+            data: {
+                message: this.message
+            }
+          }))
+          this.message = ''
+        },
+        vote(boool) {
+          window.socket.send(JSON.stringify({
+            type: "VOTE",
+            data: {
+                vote: boool
+            }
+          }))
+          this.myVote = boool
+        },
+        removeDuplicates(array) {
+            console.log('JEFF:',  array)
+            return array.filter(
+                (elem, index, self) =>
+                    self.findIndex(item => {
+                        return item.message === elem.message
+                    }) === index
+            )
+        },
+        niceTime(time) {
+			moment.updateLocale('en', {
+				relativeTime: {
+					future: '1s',
+					past: '%s',
+					s: '1s',
+					m: '1m',
+					mm: '%dm',
+					h: '1hr',
+					hh: '%dhr',
+					d: '1d',
+					dd: '%dd',
+					M: '1 month',
+					MM: '%d months',
+					y: '1 year',
+					yy: '%d years'
+				}
+			})
+			return moment(time).fromNow()
+		},
+    },
+    computed: {
+    }
+}
+</script>
+
+
+
+<style lang="scss">
+.container_wide {
+    margin: 0 40px ;
+        display: flex;
+    flex-direction: column;
+        height: calc(100vh - 10px);
+}
+.room .biginput, .room .biginput input {
+    background-color: #f5f5f5;
+}
+.room {
+
+    .logo {
+        margin: 18px 0px;
+    }
+}
+.vote {
+    position: absolute;
+    right: 40px;
+    top: 5px;
+    .button {
+        margin-left: 20px;
+    }
+}
+.message_container {
+    margin-top: 25px;
+    background: #f9f9f9;
+    padding: 20px 0;
+    width: 100%;
+    height: calc(100vh - 360px);
+    overflow: scroll;
+    * {
+        box-sizing: border-box;
+    }
+}
+.message_body {
+    width: 100%;
+    display: flex;
+    padding: 3px;
+    margin-left: 30px;
+        text-align: left;
+    line-height: 28px;
+    padding-right: 60px;
+    * {
+    }
+    .username {
+     font-size: 1.3rem;
+    font-weight: bold;
+    margin: -1px 20px 0px 20px;
+    }
+    .message {
+
+    }
+}
+.alert_message {
+    font-weight: bold;
+    margin: 10px 30px;
+    color: #6e6e6e;
+    opacity: 0.8;
+    .message {
+        font-size: 1.3rem;
+    }
+}
+.bottom_padding {
+height: 50px;
+}
+.messageBox {
+    margin: auto;
+    width: 85%;
+    margin-top: -40px;
+    background: #f1f1f1 !important;
+    padding: 0;
+    input {
+        background: #f1f1f1 !important
+    }
+}
+</style>
